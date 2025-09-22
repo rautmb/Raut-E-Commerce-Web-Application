c074ac9cd16c9f2b8eddb15a6e0cfa6bf0edde89
Machindra1846 <m.raut@optraventures.com>
2025-09-22 11:45:48 +0530

    Contact page added

--- src/Contact.js
+++ src/Contact.js
@@ -34,9 +34,22 @@ const Contact = () => {
   `;
 
   return (
-    <>
-    <h1>This is a contact page</h1>
-    </>
+    <Wrapper>
+      <h1 className="common-heading">Contact page</h1>
+
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3782.485193574975!2d73.9508679750139!3d18.55215198254755!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2c3eceadff139%3A0x4934b6f15755efed!2sEon%20IT%20Park%202!5e0!3m2!1sen!2sin!4v1758519960661!5m2!1sen!2sin" width="100%" height="400" style={{ border: 0 }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+      <div className="container">
+        <div className="contact-form">
+          <form action="https://formspree.io/f/xeorjzee" method="POST" className="contact-inputs">
+            <input type="text" placeholder="Username" name="Username" autoCapitalize="off" required />
+            <input type="email" placeholder="Email" name="Email" autoCapitalize="off" required />
+            <textarea type="text" name="Message" placeholder="Message" rows="8" cols="10" autoCapitalize="off" required ></textarea>
+            <input type="submit" value="Send" />
+          </form>
+        </div>
+      </div>
+    </Wrapper>
   )
 };
 
