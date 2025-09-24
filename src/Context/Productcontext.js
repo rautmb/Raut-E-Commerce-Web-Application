7f042745888dd7183779ff07d5c8b6ff7ed4b24b
Machindra1846 <m.raut@optraventures.com>
2025-09-24 12:34:13 +0530

    add context api and reducer

--- src/Context/Productcontext.js
+++ src/Context/Productcontext.js
@@ -1,12 +1,45 @@
-import React, { createContext, useContext } from "react";
+import axios from "axios";
+import React, { createContext, useContext, useEffect, useReducer } from "react";
+import reducer from "../reducer/productreducer"
+import { type } from "@testing-library/user-event/dist/type";
 
 const Appcontext = createContext();
 
+const API = "https://api.pujakaitem.com/api/products";
+
+const initialstate = {
+    isLoading: false,
+    isError: false,
+    Products: [],
+    FetureProducts: []
+}
+
 const Approvider = ({ children }) => {
+
+    const [state, dispatch] = useReducer(reducer, initialstate);
+
+    const getproducts = async (url) => {
+        dispatch({ type: "LOADING_DATA" });
+        try {
+            const res = await axios.get(url);
+            const products = await res.data;
+            console.log(products);
+            dispatch({ type: "SEND_DATA", payload: products });
+        }
+        catch (error) {
+            dispatch({ type: "ERROR" });
+        }
+
+    }
+
+    useEffect(() => {
+        getproducts(API);
+    }, []);
+
     let myname = "test";
     return (
         <>
-            <Appcontext.Provider value={myname}>
+            <Appcontext.Provider value={{ ...state }}>
                 {children}
             </Appcontext.Provider>
         </>
---
+++ src/reducer/productreducer.js
@@ -0,0 +1,32 @@
+const Produtreducer = (state, action) => {
+    switch (action.type) {
+        case "LOADING_DATA":
+            return {
+                ...state,
+                isLoading: true,
+            }
+
+        case "SEND_DATA":
+            const featureproductdata = action.payload.filter((v, i) => {
+                return v.fetured === true;
+            })
+            return {
+                ...state,
+                isLoading: false,
+                Products: action.payload,
+                FetureProducts: featureproductdata
+            }
+
+        case "ERROR":
+            return {
+                ...state,
+                isError: true,
+            }
+
+        default:
+            return state;
+
+    }
+}
+
+export default Produtreducer;
